2132942628525b40b3366a79405ab2c90439601e
Sergey Lisov <sleirsgoevy@gmail.com>
2020-07-24 21:00:58 +0300

    No comments

--- src/payloads/lib/syscalls.py
+++ src/payloads/lib/syscalls.py
@@ -1,19 +1,47 @@
+import urllib.request, http.client, html
+
+def get_freebsd_syscalls():
+    data = urllib.request.urlopen('https://raw.githubusercontent.com/freebsd/freebsd/stable/9/sys/kern/syscalls.master').read().decode('ascii')
+    data = '\n'.join(i.strip() for i in data.split('\n') if not i.startswith(';')).replace('\\\n', ' ')
+    assert data.startswith('$FreeBSD$\n')
+    data = data[10:]
+    ans = {}
+    for i in data.split('\n'):
+        i = i.strip()
+        if not i or 'STD' not in i.split() or i.startswith('#include'): continue
+        name = i.split('(', 1)[0].split()[-1]
+        idx = int(i.split()[0])
+        ans[idx] = name
+    return ans
+
+def get_sony_syscalls():
+    cli = http.client.HTTPSConnection('www.psdevwiki.com')
+    cli.request('GET', '/ps4/edit/Syscalls')
+    r = cli.getresponse()
+    data = html.unescape(r.read().decode('latin-1').split('<textarea ', 1)[1].split('</textarea>', 1)[0])
+    ans = {}
+    for i in data.split('\n'):
+        if i.startswith('| '):
+            try:
+                syscno, fw, syscname, proto, notes = i[2:].split(' || ')
+                syscno = int(syscno)
+            except ValueError: continue
+            if syscname.startswith('sys_'):
+                ans[syscno] = syscname[4:]
+    return ans
+
 def get_syscalls():
     ans = {}
-    with open('../../8cc/build/syscall_names.txt') as file:
-        for line in file:
-            line = line.strip()
-            if not line: continue
-            line = line.split()
-            assert line[0] == 'var' and line[1].endswith('_addr') and line[2] == '=' and line[3].startswith('sys_') and line[3].endswith('_addr;')
-            ans[line[1][:-5]] = int(line[3][4:-6])
+    ans.update(get_freebsd_syscalls())
+    ans.update(get_sony_syscalls())
     return ans
 
 print('section .text')
 print('use64')
 print()
 
-for idx, name in sorted((j, i) for i, j in get_syscalls().items()):
+for idx, name in sorted(get_syscalls().items()):
+    if '#' in name: continue
     print('global', name)
     print(name+':')
     print('mov rax,', idx)
